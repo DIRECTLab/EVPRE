d3b93d839b16f762c4c305bf2cf5fda49c306c2e
Bridger Jones <bridgerjones15@gmail.com>
2021-08-03 07:50:46 -0600

    cleaning up the config file

--- config.py
+++ config.py
@@ -4,6 +4,10 @@ class Config:
 
     def __init__(self):
         load_dotenv()
+        # Map and Graph Configuration
         self.starting_coord = (41.740563, -111.813910)
-        self.google_maps_key = os.getenv("GOOGLE_MAPS_KEY")
         self.distance = 50000
+        # API keys
+        self.google_maps_key = os.getenv("GOOGLE_MAPS_KEY")
+        self.weather_key = os.getenv("WEATHER_KEY")
+        self.traffic_key = os.getenv("TRAFFIC_KEY")
