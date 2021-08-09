2abf95fbcc9c3db2d486cca8348d525d36772b1b
Max Clark <clark.max.a@gmail.com>
2021-08-09 13:48:06 -0600

    Fixed Fastsim integration and added example to main

--- route_estimator/models/fastsim_model.py
+++ route_estimator/models/fastsim_model.py
@@ -5,11 +5,13 @@ from fastsim import simdrive
 from fastsim import vehicle
 from fastsim import cycle
 
-class fastsim_model:
-    def __init__(self):
-        pass
 
-    def get_consumed_kwh_fastsim(self, fastsim_vehicle_model_csv_path: str, avg_speed_mps: float, grade: float, distance_m: float) -> float:
+class fastsim_energy_model:
+    def __init__(self, fastsim_vehicle_csv_path: str, fastsim_vehicle_csv_index: int):
+        self.fastsim_vehicle_csv_path = fastsim_vehicle_csv_path
+        self.fastsim_vehicle_csv_index = fastsim_vehicle_csv_index
+
+    def get_consumed_kwh_fastsim(self, avg_speed_mps: float, grade: float, distance_m: float) -> float:
         # FASTSim likes a gentle increase, so we will build a ramp in speed that will be removed from the final equation
         cyc_mps_builder = []
         cur_speed = 0
@@ -19,7 +21,7 @@ class fastsim_model:
 
         # Add an extra 10 seconds to stabilize calculation (not sure if necessary)
         for x in range(10):
-            cyc_mps_builder.append(avg_speed_mps)
+            cyc_mps_builder.append(cur_speed)
 
         # The index to start calculating power usage
         start_index = len(cyc_mps_builder)
@@ -28,13 +30,23 @@ class fastsim_model:
         cyc_mps_builder.append(avg_speed_mps)
 
         # Create a constant velocity array in meters/sec until we've covered all the distance
-        # NOTE: This will overestimate by a distance of 0 - avg_speed_mps. 
+        # NOTE: This will overestimate by a distance of 0 - avg_speed_mps.
         while(distance_traveled < distance_m):
             distance_traveled += avg_speed_mps
             cyc_mps_builder.append(avg_speed_mps)
-        
+
+        stop_index = len(cyc_mps_builder)
+
+        while cur_speed > 0:
+            cyc_mps_builder.append(cur_speed)
+            cur_speed = max(0, cur_speed - 1.0)
+
+        # Add an extra 10 seconds to stabilize calculation (not sure if necessary)
+        for x in range(10):
+            cyc_mps_builder.append(cur_speed)
+
         # Create a FASTSim cycle from the data
-        cyc_mps = np.array( cyc_mps_builder)
+        cyc_mps = np.array(cyc_mps_builder)
         cyc_secs = np.array([x for x in range(len(cyc_mps))])
         cyc_grade = np.array([grade for x in range(len(cyc_mps))])
 
@@ -47,19 +59,21 @@ class fastsim_model:
             'cycMps': cyc_mps,
             'cycGrade': cyc_grade
         }
-        
+
         cyc = cycle.Cycle(cyc_dict=cycle_dict)
         cyc_jit = cyc.get_numba_cyc()
-        
-        veh = vehicle.Vehicle(1, fastsim_vehicle_model_csv_path)
-        # veh.regenA = 50
-        print(veh.Scenario_name)
-        # print(veh.vehKg)
+
+        veh = vehicle.Vehicle(self.fastsim_vehicle_csv_index,
+                              self.fastsim_vehicle_csv_path)
+
+        # print(veh.Scenario_name)
+
         veh_jit = veh.get_numba_veh()
 
         sim_drive = simdrive.SimDriveJit(cyc_jit, veh_jit)
-        sim_drive.sim_drive(1) 
-        sim_power_kwh = sim_drive.essCurKwh[start_index] - sim_drive.essCurKwh[-1]
+        sim_drive.sim_drive(1)
+        sim_power_kwh = sim_drive.essCurKwh[start_index] - \
+            sim_drive.essCurKwh[stop_index]
 
         # print(f'Sim calculated kWh loss: {sim_power}')
 
@@ -67,4 +81,4 @@ class fastsim_model:
 
 
 focus_csv = str(list(pathlib.Path(os.getcwd()).rglob('*fastsim_focus.csv'))[0])
-fastsim_model().get_consumed_kwh_fastsim(focus_csv, 15, 0.1, 100.0)
+fastsim_energy_model(focus_csv, 1).get_consumed_kwh_fastsim(15, 0.1, 100.0)
--- route_estimator/route_estimator.py
+++ route_estimator/route_estimator.py
@@ -1,15 +1,15 @@
-import sys
 import networkx as nx
 import osmnx as ox
 import geopandas as gpd
 import numpy as np
 from ipyleaflet import *
 from shapely.geometry import LineString, mapping
-from simple_energy_model import ev_power_model
+from simple_energy_model import ev_energy_model
+from fastsim_model import fastsim_energy_model
 from weather import get_weather_data
 
-class RouteEstimator:
 
+class RouteEstimator:
     def __init__(self, config):
         self.starting_coord = config.starting_coord
         self.google_maps_key = config.google_maps_key
@@ -19,36 +19,45 @@ class RouteEstimator:
 
         # create an instance of the graph
         self.create_graph()
-        
+
         # create path layer list
         self.path_layer_list = []
-        
-        # default simple energy model vehicle
-        self.vehicle = ev_power_model(config.default_ev_model['mass'], config.default_ev_model['air_resistance'], config.default_ev_model['area'])
-        
-        
-        #create the map from ipyleaflet
-        self.m = Map(center=self.starting_coord, basemap=basemaps.CartoDB.Positron, zoom=15)
 
+        if config.model == "FASTSIM":
+            # Fastsim model
+            print("JITing model, please wait...")
+            self.vehicle = fastsim_energy_model(
+                config.fastsim_vehicle_csv_path, config.fastsim_vehicle_csv_index)
+        else:
+            # default simple energy model vehicle
+            self.vehicle = ev_energy_model(
+                config.default_ev_model['mass'], config.default_ev_model['air_resistance'], config.default_ev_model['area'])
+
+        # create the map from ipyleaflet
+        self.m = Map(center=self.starting_coord,
+                     basemap=basemaps.CartoDB.Positron, zoom=15)
 
     def create_map(self):
         # create the TO marker style
-        to_marker_style = AwesomeIcon(name='circle',icon_color='white',marker_color='red',spin=False)
-        #create the two markers
+        to_marker_style = AwesomeIcon(
+            name='circle', icon_color='white', marker_color='red', spin=False)
+        # create the two markers
         from_marker = Marker(location=self.starting_coord)
         to_marker = Marker(location=self.starting_coord, icon=to_marker_style)
-        
-        from_marker.observe(lambda event: self.handle_change_location(event, to_marker), 'location')
-        to_marker.observe(lambda event: self.handle_change_location(event, from_marker), 'location')
+
+        from_marker.observe(lambda event: self.handle_change_location(
+            event, to_marker), 'location')
+        to_marker.observe(lambda event: self.handle_change_location(
+            event, from_marker), 'location')
         self.m.add_layer(from_marker)
         self.m.add_layer(to_marker)
         self.set_nearest_node(from_marker)
         self.set_nearest_node(to_marker)
         return self.m
 
-
     def create_graph(self):
-        graph = ox.graph_from_point(self.starting_coord, self.distance, network_type="drive")
+        graph = ox.graph_from_point(
+            self.starting_coord, self.distance, network_type="drive")
         graph = ox.add_edge_speeds(graph)
         graph = ox.add_edge_bearings(graph)
         graph = ox.add_edge_travel_times(graph)
@@ -69,38 +78,77 @@ class RouteEstimator:
             return ox.graph_to_gdfs(self.graph)
         except:
             print("Graph has not been created")
-            
+
     def handle_change_location(self, event, marker):
         event_owner = event['owner']
-        event_owner.nearest_node = ox.get_nearest_node(self.graph, event_owner.location)
+        event_owner.nearest_node = ox.get_nearest_node(
+            self.graph, event_owner.location)
         marker.neares_node = ox.get_nearest_node(self.graph, marker.location)
-        shortest_path = nx.bellman_ford_path(self.graph, event_owner.nearest_node, marker.neares_node, weight=self.edge_weight)
+        shortest_path = nx.bellman_ford_path(
+            self.graph, event_owner.nearest_node, marker.neares_node, weight=self.edge_weight)
 
         if len(self.path_layer_list) == 1:
             self.m.remove_layer(self.path_layer_list[0])
             self.path_layer_list.pop()
 
         shortest_path_points = self.nodes.loc[shortest_path]
-        path = gpd.GeoDataFrame([LineString(shortest_path_points.geometry.values)], columns=['geometry'])
-        path_layer = GeoData(geo_dataframe=path, style={'color':'black', 'weight':2})
+        path = gpd.GeoDataFrame(
+            [LineString(shortest_path_points.geometry.values)], columns=['geometry'])
+        path_layer = GeoData(geo_dataframe=path, style={
+                             'color': 'black', 'weight': 2})
         self.m.add_layer(path_layer)
         self.path_layer_list.append(path_layer)
-        
+
     def set_nearest_node(self, marker):
-        marker.nearest_node = ox.distance.nearest_nodes(self.graph, marker.location[0], marker.location[1])
+        marker.nearest_node = ox.distance.nearest_nodes(
+            self.graph, marker.location[0], marker.location[1])
         return
-    
-    
-    def activate_simple_energy_model(self):
+
+    def activate_energy_model(self):
+        if isinstance(self.vehicle, ev_energy_model):
+            self.__activate_simple_energy_model()
+        elif isinstance(self.vehicle, fastsim_energy_model):
+            self.__activate_fastsim_energy_model()
+
+    def __activate_fastsim_energy_model(self):
         # get a dataframe version of the graph to modify
         graphDF = self.get_dataframe_from_graph()
-        
+
         # placeholder list for new column on the graph
         energy_consumed = []
-        
+
+        # For each row, grab the required value, compute the energy, and add to the energy list
+        for index, row in graphDF[1].iterrows():
+            speed_mps = row['speed_kph'] / 3.6
+            grade = row['grade']
+            length = row['length']
+            energy_consumption = self.vehicle.get_consumed_kwh_fastsim(
+                speed_mps, grade, length)
+            energy_consumed.append(energy_consumption)
+
+        # create a new column on the graphDF
+        graphDF[1]['fastsim_model_e'] = energy_consumed
+
+        # convert df back into graph and assign modified graph to self.graph
+        self.graph = ox.graph_from_gdfs(graphDF[0], graphDF[1])
+
+        # update the node, edges objects
+        self.nodes, self.edges = ox.graph_to_gdfs(self.graph)
+
+        # update the map mode to do shortest path based on the simple energy model
+        self.edge_weight = 'fastsim_model_e'
+
+    def __activate_simple_energy_model(self):
+        # get a dataframe version of the graph to modify
+        graphDF = self.get_dataframe_from_graph()
+
+        # placeholder list for new column on the graph
+        energy_consumed = []
+
         # get the starting wind and wind bearing for the energy consumption model
-        current_weather = get_weather_data(self.starting_coord[0], self.starting_coord[1], self.weather_key)
-        
+        current_weather = get_weather_data(
+            self.starting_coord[0], self.starting_coord[1], self.weather_key)
+
         # For each row, grab the required value, compute the energy, and add to the energy list
         for index, row in graphDF[1].iterrows():
             speed = row['speed_kph']
@@ -109,17 +157,18 @@ class RouteEstimator:
             length = row['length']
             wind_speed = current_weather['wind_speed']
             wind_heading = current_weather['wind_heading']
-            energy_consumption = self.vehicle.energy_consumption(grade, speed/3.6, 0, wind_speed, car_heading=bearing, wind_h=wind_heading, length=length)
+            energy_consumption = self.vehicle.energy_consumption(
+                grade, speed/3.6, 0, wind_speed, car_heading=bearing, wind_h=wind_heading, length=length)
             energy_consumed.append(energy_consumption)
-        
+
         # create a new column on the graphDF
         graphDF[1]['simple_model_e'] = energy_consumed
-        
+
         # convert df back into graph and assign modified graph to self.graph
         self.graph = ox.graph_from_gdfs(graphDF[0], graphDF[1])
-        
+
         # update the node, edges objects
         self.nodes, self.edges = ox.graph_to_gdfs(self.graph)
-        
-        #update the map mode to do shortest path based on the simple energy model
+
+        # update the map mode to do shortest path based on the simple energy model
         self.edge_weight = 'simple_model_e'
