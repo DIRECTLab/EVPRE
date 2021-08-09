362eb0ad9f623360e59d4a2b0f72c56195682efb
Max Clark <clark.max.a@gmail.com>
2021-08-09 13:47:27 -0600

    Renamed items for clarity (ev energy vs ev power) Formatted

--- route_estimator/models/simple_energy_model.py
+++ route_estimator/models/simple_energy_model.py
@@ -1,18 +1,14 @@
-#import config #This is your api keys openroute, weatherapi, and google(google is a pain to set up FYI)
+# import config #This is your api keys openroute, weatherapi, and google(google is a pain to set up FYI)
 from math import sin, radians, cos
-import sqlite3 as sq
-import pandas as pd
-import numpy as np
-import requests
-from os import sys
 
-class ev_power_model:
+
+class ev_energy_model:
     def __init__(self, mass, air_resistance, area, rf=0.02, air_density=1.225):
-        self.mass = mass # in kilograms かな... 
+        self.mass = mass  # in kilograms かな...
         self.air_resistance = air_resistance
-        self.area = area 
+        self.area = area
         self.rolling_friction = rf
-        self.air_density = air_density # kg/m^3
+        self.air_density = air_density  # kg/m^3
         self.gravity = 9.8
 
     def convert_heading(self, heading):
@@ -27,13 +23,12 @@ class ev_power_model:
 
         headings = ["E", "NE", "N", "NW", "W", "SW", "S", "SE"]
         angle = 0
-        
+
         for h in headings:
             if heading is h:
                 break
             angle += 45
         return angle
-
 
     def energy_consumption(self, angle, v_car, a, v_wind, car_heading=0, wind_h=0, length=1):
         """
@@ -52,25 +47,28 @@ class ev_power_model:
 
         """
 
-        #calculate wind velocity, believing that the y-direction of the vehicle is always the y-axis.
+        # calculate wind velocity, believing that the y-direction of the vehicle is always the y-axis.
         # the weather api returns it in degrees. no need to convert
         # wind_heading = self.convert_heading(wind_h)
         wind_heading = wind_h
         wind_heading -= car_heading
-        
+
         wind_speed = v_car - v_wind * cos(radians(wind_heading))
 
         calc1 = self.mass * a
 
-        calc2 = self.mass * self.gravity * length * (1.75/1000)*(0.0328*v_car+4.575)
+        calc2 = self.mass * self.gravity * \
+            length * (1.75/1000)*(0.0328*v_car+4.575)
 
-        calc3 = 0.5 * self.air_density * self.area * self.air_resistance * (wind_speed ** 2)
+        calc3 = 0.5 * self.air_density * self.area * \
+            self.air_resistance * (wind_speed ** 2)
 
         calc4 = self.mass * self.gravity * sin(radians(angle))
 
-        energy = ( ((calc1 + calc2 + calc3 + calc4) * v_car)/0.92/0.91) 
+        energy = (((calc1 + calc2 + calc3 + calc4) * v_car)/0.92/0.91)
 
         return energy
+
 
 def equalizer_constant(energy1, energy2):
     """
@@ -87,8 +85,9 @@ def equalizer_constant(energy1, energy2):
         equalizer = [100 for a in range(len(energy1))]
         change = 50
         while difference > 0.009 or difference < -0.009:
-            add_diff =  (energy2[x] * (equalizer[x] + change)) - energy1[x]
-            sub_diff = (energy2[x] * (equalizer[x] - change) - energy1[x]) if (energy2[x] * (equalizer[x] - change) - energy1[x]) > 0 else (energy2[x] * (equalizer[x] - change) - energy1[x]) * -1
+            add_diff = (energy2[x] * (equalizer[x] + change)) - energy1[x]
+            sub_diff = (energy2[x] * (equalizer[x] - change) - energy1[x]) if (energy2[x] * (
+                equalizer[x] - change) - energy1[x]) > 0 else (energy2[x] * (equalizer[x] - change) - energy1[x]) * -1
             if sub_diff < add_diff:
                 difference = sub_diff
                 equalizer[x] -= change
@@ -96,17 +95,18 @@ def equalizer_constant(energy1, energy2):
                 difference = add_diff
                 equalizer[x] += change
             change /= 2
-         
+
     return equalizer
+
 
 if __name__ == "__main__":
 
-    focus = ev_power_model(1337, 0.28, 1.825 * 1.469)
-    f150  = ev_power_model(1500, 0.28, 1.825 * 1.469)
+    focus = ev_energy_model(1337, 0.28, 1.825 * 1.469)
+    f150 = ev_energy_model(1500, 0.28, 1.825 * 1.469)
 
     #angle = [0,-0.01,-0.05,-0.07,-0.10]
     #v = [38,38,38,38,39]
-    #accel = [0.13,0.13,0.13,0.13,0.13] 
+    #accel = [0.13,0.13,0.13,0.13,0.13]
     angle = [0]
     v = [27.8]
     accel = [.000010030864197531]
