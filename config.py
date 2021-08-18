a1caee782126f9ff049d1e17360b876cbe6d9d78
Chris-Kropp <christopherakropp@gmail.com>
2021-08-18 14:36:41 -0600

    reduced range

--- config.py
+++ config.py
@@ -20,7 +20,8 @@ class Config:
 
         # Map and Graph Configuration------------------------
         self.starting_coord = (41.740563, -111.813910)
-        self.distance = 250000
+        # May need to increase for full range estimate
+        self.distance = 50000
         self.default_edge_weight = "length"
 
         # API keys--------------------------------------------
